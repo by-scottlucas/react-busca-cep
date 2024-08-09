dea8d60c2c14cdf463e25bd4978e140cb5ca5a67
Lucas S. Silva <134394867+by-scottlucas@users.noreply.github.com>
2024-08-09 15:55:02 -0300

    Update LICENCE

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,11 +1,68 @@
-import './Map.css';
-import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-import { useEffect } from 'react';
+import 'leaflet/dist/leaflet.css';
+import { useEffect, useState } from 'react';
+import './Map.css';
+import Modal from '../Modal/Modal';
 
 export default function Map({ cep }) {
+  const [modalMessage, setModalMessage] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [coordinates, setCoordinates] = useState(null);
+
   useEffect(() => {
-    const map = L.map('map').setView([-24.0058, -46.4025], 13);
+    if (!cep) return;
+
+    async function fetchAddress() {
+      try {
+        const viaCepUrl = `https://viacep.com.br/ws/${cep}/json/`;
+        const response = await fetch(viaCepUrl);
+        const data = await response.json();
+
+        if (data.erro) {
+          setModalMessage('CEP não encontrado.');
+          setShowModal(true);
+          return;
+        }
+
+        const address = `${data.logradouro}, ${data.bairro}, ${data.localidade} - ${data.uf}`;
+        fetchCoordinates(address);
+      } catch (error) {
+        console.error('Erro ao buscar o endereço:', error);
+        setModalMessage('Erro ao buscar o endereço.');
+        setShowModal(true);
+      }
+    }
+
+    async function fetchCoordinates(address) {
+      try {
+        const apiUrl = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(address)}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        if (data.length === 0) {
+          setModalMessage('Endereço não encontrado.');
+          setShowModal(true);
+          return;
+        }
+
+        setCoordinates({
+          latitude: parseFloat(data[0].lat),
+          longitude: parseFloat(data[0].lon),
+        });
+      } catch (error) {
+        console.error('Erro ao buscar coordenadas:', error);
+        setModalMessage('Erro ao buscar coordenadas.');
+        setShowModal(true);
+      }
+    }
+
+    fetchAddress();
+  }, [cep]);
+
+  useEffect(() => {
+    if (!coordinates) return;
+
+    const map = L.map('map').setView([coordinates.latitude, coordinates.longitude], 18);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 20,
@@ -18,77 +75,33 @@ export default function Map({ cep }) {
       iconAnchor: [12, 41],
       popupAnchor: [1, -34],
       shadowSize: [41, 41],
-      className: 'custom-marker'
+      className: 'custom-marker',
     });
 
-    let marker = null;
-
-    function buscarEnderecoPorCEP() {
-      const viaCepUrl = `https://viacep.com.br/ws/${cep}/json/`;
-      console.log('URL do ViaCEP:', viaCepUrl);
-
-      fetch(viaCepUrl)
-        .then(response => response.json())
-        .then(data => {
-          console.log('Resposta do ViaCEP:', data);
-          if (!data.erro) {
-            const endereco = `${data.logradouro}, ${data.bairro}, ${data.localidade} - ${data.uf}`;
-            buscarCoordenadasPorEndereco(endereco);
-          } else {
-            alert('CEP não encontrado.');
-          }
-        })
-        .catch(error => {
-          console.error('Erro ao buscar endereço pelo CEP:', error);
-          console.log('Ocorreu um erro ao buscar o endereço.');
-        });
-    }
-
-    function buscarCoordenadasPorEndereco(endereco) {
-      var apiUrl = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(endereco)}`;
-      fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-          if (data.length > 0) {
-            var coordenadas = {
-              latitude: parseFloat(data[0].lat),
-              longitude: parseFloat(data[0].lon)
-            };
-            adicionarMarcador(coordenadas);
-            centralizarMapa(coordenadas.latitude, coordenadas.longitude);
-          } else {
-            console.log('Endereço não encontrado.');
-          }
-        })
-        .catch(error => {
-          console.error('Erro ao buscar coordenadas:', error);
-          console.log('Ocorreu um erro ao buscar as coordenadas. Verifique a conexão ou tente novamente mais tarde.');
-        });
-    }
-
-    function centralizarMapa(latitude, longitude) {
-      map.setView([latitude, longitude], 18);
-    }
-
-    function adicionarMarcador(coordenadas) {
-      if (marker) {
-        map.removeLayer(marker);
-      }
-      marker = L.marker([coordenadas.latitude, coordenadas.longitude], { icon: customIcon }).addTo(map);
-    }
-
-    if (cep) {
-      buscarEnderecoPorCEP();
-    }
+    L.marker([coordinates.latitude, coordinates.longitude], { icon: customIcon }).addTo(map);
 
     return () => {
       map.remove();
     };
-  }, [cep]);
+  }, [coordinates]);
+
+  function closeModal() {
+    setShowModal(false);
+    setModalMessage('');
+  }
 
   return (
     <div className="main">
-      <div id="map" className="map-container"></div>
+      {coordinates ?
+        <div id="map" className="map-container"></div> :
+        <div className="map-container">
+          <p className='message-loading'>Carregando Mapa. . .</p>
+        </div>
+      }
+      <Modal show={showModal}>
+        <h3>{modalMessage}</h3>
+        <button onClick={closeModal}>OK</button>
+      </Modal>
     </div>
   );
 }
--- src/services/cep.service.jsx
+++ src/services/cep.service.jsx
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-// 01001000/json/
-
 const api = axios.create({
     baseURL: "https://viacep.com.br/ws/"
 })
